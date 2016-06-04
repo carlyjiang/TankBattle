6a0601b39c1ba38cc7b75415fd357bb9b527ba0c
wyjzhi89 <Marc@guest-wireless-nup-1617-10-121-023-029.usc.edu>
2016-06-04 13:24:04 -0700

    nav mashed single player trancing

---
+++ Tanks/Assets/Navi.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navi : MonoBehaviour {
+
+	NavMeshAgent nav;
+	GameObject[] players;
+
+	// Use this for initialization
+	void Awake () {
+	
+		players = GameObject.FindGameObjectsWithTag ("Player");
+
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject p in players)
+			nav.SetDestination (p.transform.position);
+	}
+}
